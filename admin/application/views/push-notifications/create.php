81215408d9149e5a71e19e2e94aff0df20d58bc5
Mahamad Ali <manknojiya121@gmail.com>
2022-05-26 18:18:49 +0530

    api changes

--- admin/application/views/push-notifications/create.php
+++ admin/application/views/push-notifications/create.php
@@ -7,7 +7,7 @@
             <div class="card-header">
               <div class="row align-items-center">
                 <div class="col-8">
-                  <h3 class="mb-0">Add Merchant</h3>
+                  <h3 class="mb-0">Send Notification</h3>
                 </div>
               </div>
             </div>
